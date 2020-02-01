7310e4a43f19c3c3c495c0d092e3af79ce851079
0scar <qgt268@alumni.ku.dk>
2020-02-01 13:24:24 +0100

    Add ascii char defintions

---
+++ src/ascii.h
@@ -0,0 +1,131 @@
+#define ASCII_NULL                  0
+#define ASCII_SOH                   1                   // start of heading
+#define ASCII_STX                   2                   // start of text
+#define ASCII_ETX                   3                   // end of text
+#define ASCII_EOT                   4                   // end of transmission
+#define ASCII_ENQ                   5                   // enquiry
+#define ASCII_ACK                   6                   // acknowledgement
+#define ASCII_BEL                   7                   // bell
+#define ASCII_BS                    8                   // backspace
+#define ASCII_HT                    9                   // horizontal tab
+#define ASCII_LF                    10                  // line feed '\r'
+#define ASCII_VT                    11                  // vertical tab
+#define ASCII_FF                    12                  // form feed
+#define ASCII_CR                    13                  // carriage return '\n'
+#define ASCII_SO                    14                  // shift out
+#define ASCII_SI                    15                  // shift in
+#define ASCII_DLE                   16                  // data link escape (??)
+#define ASCII_DC1                   17                  // device contorl 1
+#define ASCII_DC2                   18                  // ...
+#define ASCII_DC3                   19                  // ...
+#define ASCII_DC4                   20                  //                4
+#define ASCII_NAK                   21                  // !ack
+#define ASCII_SYN                   22                  // synchhonous idle
+#define ASCII_ETB                   23                  // end of transmission block
+#define ASCII_CAN                   24                  // cancel
+#define ASCII_EM                    25                  // end of medium
+#define ASCII_SUB                   26                  // substitude
+#define ASCII_ESC                   27                  //
+#define ASCII_FS                    28                  // file seperator
+#define ASCII_GS                    29                  // group seperator
+#define ASCII_RS                    30                  // record seperator
+#define ASCII_US                    31                  // unit seperator
+
+#define ASCII_SPACE                 32
+#define ASCII_EXCLAMATION           33
+#define ASCII_QUOTES                34
+#define ASCII_NUMBER                35
+#define ASCII_DOLLAR                36
+#define ASCII_PERCENT               37
+#define ASCII_AMPERSAND             38
+#define ASCII_QUOTE                 39
+#define ASCII_PARENTHESISL          40
+#define ASCII_PARENTHESISR          41
+#define ASCII_ASTERISK              42
+#define ASCII_PLUS                  43
+#define ASCII_COMMA                 44
+#define ASCII_HYPHEN                45
+#define ASCII_PERIOD                46
+#define ASCII_SLASH                 47
+#define ASCII_0                     48
+#define ASCII_1                     49
+#define ASCII_2                     50
+#define ASCII_3                     51
+#define ASCII_4                     52
+#define ASCII_5                     53
+#define ASCII_6                     54
+#define ASCII_7                     55
+#define ASCII_8                     56
+#define ASCII_9                     57
+#define ASCII_COLON                 58
+#define ASCII_SEMICOLON             59
+#define ASCII_LESS                  60
+#define ASCII_EQUAL                 61
+#define ASCII_GREATER               62
+#define ASCII_QUESTION              63
+
+#define ASCII_AT                    64
+#define ASCII_A                     65
+#define ASCII_B                     66
+#define ASCII_C                     67
+#define ASCII_D                     68
+#define ASCII_E                     69
+#define ASCII_F                     70
+#define ASCII_G                     71
+#define ASCII_H                     72
+#define ASCII_I                     73
+#define ASCII_J                     74
+#define ASCII_K                     75
+#define ASCII_L                     76
+#define ASCII_M                     77
+#define ASCII_N                     78
+#define ASCII_O                     79
+#define ASCII_P                     80
+#define ASCII_Q                     81
+#define ASCII_R                     82
+#define ASCII_S                     83
+#define ASCII_T                     84
+#define ASCII_U                     85
+#define ASCII_V                     86
+#define ASCII_W                     87
+#define ASCII_X                     88
+#define ASCII_Y                     89
+#define ASCII_Z                     90
+#define ASCII_BRACKETL              91
+#define ASCII_BACKSLASH             92
+#define ASCII_BRACKETR              93
+#define ASCII_CARET                 94
+#define ASCII_UNDERSCORE            95
+
+#define ASCII_GRAVE                 96
+#define ASCII_a                     97
+#define ASCII_b                     98
+#define ASCII_c                     99
+#define ASCII_d                     100
+#define ASCII_e                     101
+#define ASCII_f                     102
+#define ASCII_g                     103
+#define ASCII_h                     104
+#define ASCII_i                     105
+#define ASCII_j                     106
+#define ASCII_k                     107
+#define ASCII_l                     108
+#define ASCII_m                     109
+#define ASCII_n                     110
+#define ASCII_o                     111
+#define ASCII_p                     112
+#define ASCII_q                     113
+#define ASCII_r                     114
+#define ASCII_s                     115
+#define ASCII_t                     116
+#define ASCII_u                     117
+#define ASCII_v                     118
+#define ASCII_w                     119
+#define ASCII_x                     120
+#define ASCII_y                     121
+#define ASCII_z                     122
+#define ASCII_BRACEL                123
+#define ASCII_VERTICAL              124
+#define ASCII_BRACER                125
+#define ASCII_TILDE                 126
+#define ASCII_DELETE                127
